8ca5118f7e80d7abe02f1fb9a6e7f8c70a375f42
djoun52 <simon.rioux.trv@gmail.com>
2021-09-13 06:59:30 +0200

    verif

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-
+// MONGO_URI='mongodb+srv://admin:CkZel0veHftydCMk@cluster0.9jxva.mongodb.net/Cluster0?retryWrites=true&w=majority'
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
